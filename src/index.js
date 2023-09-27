eb440e914014fac6c53adf729d2dcf4d3c080827
Ben Sharkey <n00212320@iadt.ie>
2023-09-27 17:58:01 +0100

    Class 2709 Props designed

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const ModuleTitle = (props) => {
+  return <b>Welcome to the {props.name} Module!</b>
+}
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <>
+  <ModuleTitle name="Front End Development" />
+  </>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
